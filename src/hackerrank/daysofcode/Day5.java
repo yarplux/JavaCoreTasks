702ae2975a846df654815a6d674b829ec892dfbb
Rudenko Egor <36299686+yarplux@users.noreply.github.com>
2018-03-01 16:26:09 +0300

    Create Day5.java

---
+++ src/hackerrank/daysofcode/Day5.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i=1; i<=10; i++) System.out.println(n+" x "+i+" = "+n*i);
+    }
+}
