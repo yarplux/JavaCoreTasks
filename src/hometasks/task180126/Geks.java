16a922d63b3f3275b32fae1855d68732c52272f1
yarplux <yarplux@yandex.ru>
2019-01-24 17:04:16 +0300

    + текстовая задача

---
+++ src/hometasks/task180126/Geks.java
@@ -0,0 +1,48 @@
+package hometasks.task180126;
+
+
+class Geks {
+    private int x;
+    private int y;
+    private Geks[] links;
+    private int m = 0;
+    private boolean watch = true;
+
+    Geks(int ny, int nx){
+        this.x = nx;
+        this.y = ny;
+    }
+
+    void setLinks(Geks[] nlinks){
+        this.links = nlinks;
+    }
+
+    int getX(){
+        return this.x;
+    }
+
+    int getY(){
+        return this.y;
+    }
+
+    boolean getWatch() {
+        return this.watch;
+    }
+
+    int getMark () {
+        return this.m;
+    }
+
+    void setMark(int m) {
+        this.m = m;
+    }
+
+    void setWatch(boolean watched) {
+        this.watch = watched;
+    }
+
+    Geks[] getLinks(){
+        return this.links;
+    }
+
+}
